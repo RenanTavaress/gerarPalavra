1aef67143025eccd5857e3c4ae4508ef7968f412
Renan Tavares <renan_velox@hotmail.com>
2018-10-27 15:12:08 -0300

    importando modulos

--- teste/principal.py
+++ teste/principal.py
@@ -1,10 +1,12 @@
-import gerador.palavra
+import gerador.Palavra
 import base.memoria
 
 
 def main():
     quantidade = int(input("Digite a quantidade de palavras que voce deseja sortear \n"))
-    print(gerador.palavra.sortear(quantidade))
+    vogal = input("Digite a vogal\n")
+    consoante = input("consoa")
+    print(gerador.Palavra.sortear(quantidade,vogal,consoante))
 
 
 if __name__ == '__main__':
